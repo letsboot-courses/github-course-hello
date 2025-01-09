0494553cf785e149cd05c6f4e851d4f94c61f191
Tobias Denzler <tobias.denzler@evonate.ch>
2025-01-09 11:23:04 +0100

    add env variable

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ Einfache Hello World Applikation mit SpringBoot.
 mvn clean install
 ```
 
+## Run
+
+```
+mvn spring-boot:run
+```
+
+Optionally you can set the APP_GREETER environment variable.
+
+## Check
+
+```
+curl http://localhost:8080/hello
+```
--- src/main/java/ch/example/controller/HelloController.java
+++ src/main/java/ch/example/controller/HelloController.java
@@ -1,13 +1,17 @@
 package ch.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${app.greeter:Letsboot}")
+	private String greeter;
+
 	@GetMapping("/hello")
 	public String index() {
-		return "Greetings from Letsboot!";
+		return "Greetings from " + greeter + "!";
 	}
 }
