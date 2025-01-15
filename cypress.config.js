d45ae9959f2c048e3891c29a55b7892063b69533
Tobias Denzler <tobias.denzler@evonate.ch>
2025-01-15 11:35:03 +0100

    add cypress results

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
+    baseUrl: 'http://localhost:8080',
+    specPattern: 'cypress/e2e/**/*.cy.js'
   },
+  reporter: 'mocha-junit-reporter',
+  reporterOptions: {
+    mochaFile: 'cypress/results/junit/test-results-[hash].xml',
+    toConsole: true
+  }
 });
