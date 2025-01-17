54d01327d4ddfba82bf96db75198dcdf48413a7e
Tobias Denzler <tobias.denzler@evonate.ch>
2025-01-17 15:22:25 +0100

    test

--- cypress/e2e/hello.cy.js
+++ cypress/e2e/hello.cy.js
@@ -1,6 +1,7 @@
 describe('Hello Endpoint Test', () => {
     it('lädt die Begrüßung vom /hello Endpoint', () => {
-        cy.visit('http://localhost:8080'); // Öffnet die Startseite
+        const port = Cypress.env('PORT') || 8080;  // Fallback auf 8080
+        cy.visit('http://localhost:${port}'); // Öffnet die Startseite
         cy.contains('Load Greeting').click(); // Klickt auf den Button
         cy.get('#greeting').should('contain', 'Greetings from Letsboot!'); // Überprüft den Text
     });
